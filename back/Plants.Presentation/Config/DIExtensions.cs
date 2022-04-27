d5fde41630e423a94952456891242d518ec933b4
korov <korova2321@gmail.com>
2022-04-27 04:34:46 +0300

    Add cors

---
+++ back/Plants.Presentation/Config/DIExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Plants.Presentation.Config
+{
+    public static class DIExtensions
+    {
+        /// <summary>
+        /// Would bind a section, that corresponds to linear subdivisioning of 
+        /// config into sections using <param name="sectionNames"></param>
+        /// If no section names is provided, then an entire config would be used
+        /// </summary>
+        public static IServiceCollection BindConfigSection<T>(this IServiceCollection services,
+          IConfiguration config, params string[] sectionNames) where T : class
+        {
+            services.Configure<T>(options =>
+            {
+                sectionNames
+                    .Aggregate(config, (config, sectionName) => config.GetSection(sectionName))
+                    .Bind(options);
+            });
+            return services;
+        }
+    }
+}
