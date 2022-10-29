ba8934dcab15a4cd6254adf6797e927a42efde4a
korov <korova2321@gmail.com>
2022-10-29 22:19:57 +0300

    Add sample aggregate

--- src/back/Plants.Domain/AggregateBase.cs
+++ src/back/Plants.Domain/AggregateBase.cs
@@ -2,12 +2,14 @@
 
 public abstract class AggregateBase
 {
-    public AggregateBase(Guid id, string name)
+    public AggregateBase(Guid id)
     {
         Id = id;
-        Name = name;
+        Name = this.GetType().Name;
     }
 
+    public const long NewAggregateVersion = -1;
+    public long Version { get; } = NewAggregateVersion;
     public Guid Id { get; }
     public string Name { get; }
 }
