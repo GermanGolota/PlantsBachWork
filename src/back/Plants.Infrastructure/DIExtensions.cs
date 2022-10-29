32b251d5cf5a478ad1d004b38ca7bddaa9fc3d10
korov <korova2321@gmail.com>
2022-10-29 22:34:12 +0300

    Add external commands registration

--- src/back/Plants.Infrastructure/DIExtensions.cs
+++ src/back/Plants.Infrastructure/DIExtensions.cs
@@ -66,7 +66,8 @@ public static class DIExtensions
 
     private static IServiceCollection AddEventSourcing(this IServiceCollection services)
     {
-        services.AddSingleton<TypeHelper>();
+        var helper = new TypeHelper();
+        services.AddSingleton(helper);
         services.AddSingleton<CQRSHelper>();
         services.AddTransient<EventStoreConnectionFactory>();
         services.AddSingleton(factory => factory.GetRequiredService<EventStoreConnectionFactory>().Create());
@@ -74,6 +75,20 @@ public static class DIExtensions
         services.AddTransient<ICommandSender, CommandSender>();
         services.AddTransient<IEventStore, EventStoreEventStore>();
         services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+        services.RegisterExternalCommands(helper);
+        return services;
+    }
+
+    private static IServiceCollection RegisterExternalCommands(this IServiceCollection services, TypeHelper helper)
+    {
+        var baseType = typeof(ICommandHandler<>);
+        foreach (var type in helper.Types.Where(x => x.IsAssignableToGenericType(baseType) && x != baseType))
+        {
+            foreach (var @interface in type.GetInterfaces().Where(x => x.IsAssignableToGenericType(baseType)))
+            {
+                services.AddTransient(@interface, type);
+            }
+        }
         return services;
     }
 
