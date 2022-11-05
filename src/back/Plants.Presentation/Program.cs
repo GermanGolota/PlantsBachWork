f0677068fa7a2c11bd7d1c58879b07d6a6597173
korov <korova2321@gmail.com>
2022-11-06 00:05:30 +0200

    Refactor program

--- src/back/Plants.Presentation/Program.cs
+++ src/back/Plants.Presentation/Program.cs
@@ -1,14 +1,11 @@
 using Plants.Presentation;
 
-var host = CreateHostBuilder(args).Build();
-Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-host.Run();
-
-static IHostBuilder CreateHostBuilder(string[] args)
-{
-    return Host.CreateDefaultBuilder(args)
+var host = Host.CreateDefaultBuilder(args)
         .ConfigureWebHostDefaults(webBuilder =>
         {
             webBuilder.UseStartup<Startup>();
-        });
-}
+        })
+        .Build();
+
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+host.Run();
