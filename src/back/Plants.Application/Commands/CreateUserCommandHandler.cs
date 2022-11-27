8ee30cba3adbcb75b825f9c53bdd30486b7e8c6c
korov <korova2321@gmail.com>
2022-11-27 06:33:23 +0200

    Introduce string helper

--- src/back/Plants.Application/Commands/CreateUserCommandHandler.cs
+++ src/back/Plants.Application/Commands/CreateUserCommandHandler.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
 using Plants.Application.Contracts;
-using System.Security.Cryptography;
+using Plants.Shared;
 
 namespace Plants.Application.Commands;
 
@@ -18,42 +18,10 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Creat
     public async Task<CreateUserResult> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
         const int TempPasswordLength = 8;
-        var tempPassword = GetRandomAlphanumericString(TempPasswordLength);
+        var tempPassword = StringHelper.GetRandomAlphanumericString(TempPasswordLength);
         var lang = request.Language ?? "English";
         await _emailer.SendInvitationEmail(request.Email, request.Login, tempPassword, lang);
-        return await _user.CreateUser(request.Login, request.Roles, 
+        return await _user.CreateUser(request.Login, request.Roles,
             request.FirstName, request.LastName, request.PhoneNumber, tempPassword);
-    }
-
-    private static string GetRandomAlphanumericString(int length)
-    {
-        const string alphanumericCharacters =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-            "abcdefghijklmnopqrstuvwxyz" +
-            "0123456789";
-        return GetRandomString(length, alphanumericCharacters);
-    }
-
-    private static string GetRandomString(int length, IEnumerable<char> characterSet)
-    {
-        if (length < 0)
-            throw new ArgumentException("length must not be negative", "length");
-        if (length > int.MaxValue / 8) // 250 million chars ought to be enough for anybody
-            throw new ArgumentException("length is too big", "length");
-        if (characterSet == null)
-            throw new ArgumentNullException("characterSet");
-        var characterArray = characterSet.Distinct().ToArray();
-        if (characterArray.Length == 0)
-            throw new ArgumentException("characterSet must not be empty", "characterSet");
-
-        var bytes = new byte[length * 8];
-        new RNGCryptoServiceProvider().GetBytes(bytes);
-        var result = new char[length];
-        for (int i = 0; i < length; i++)
-        {
-            ulong value = BitConverter.ToUInt64(bytes, i * 8);
-            result[i] = characterArray[value % (uint)characterArray.Length];
-        }
-        return new string(result);
     }
 }
