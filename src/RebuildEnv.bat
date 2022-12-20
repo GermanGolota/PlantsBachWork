36100a511a54931ada7328c3c490875ec5adc3a1
korov <korova2321@gmail.com>
2022-12-21 00:56:45 +0200

    Update build script to accept certificates

--- src/RebuildEnv.bat
+++
@@ -1,4 +0,0 @@
-CALL docker-compose -f docker-compose-env.yml down --remove-orphans
-CALL rmdir /s /q persist
-CALL git restore persist
-CALL docker-compose -f docker-compose-env.yml up --remove-orphans
