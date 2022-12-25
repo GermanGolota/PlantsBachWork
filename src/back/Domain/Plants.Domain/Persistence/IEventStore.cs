8c737a6c5b399357df96dd74f7a85965f8d1dd10
korov <korova2321@gmail.com>
2022-12-25 21:05:48 +0200

    Add helper method for append event in eventstore

--- src/back/Domain/Plants.Domain/Persistence/IEventStore.cs
+++ src/back/Domain/Plants.Domain/Persistence/IEventStore.cs
@@ -1,4 +1,7 @@
-﻿namespace Plants.Domain.Persistence;
+﻿using Plants.Domain.Extensions;
+using Plants.Domain.Services;
+
+namespace Plants.Domain.Persistence;
 
 public interface IEventStore
 {
@@ -12,3 +15,19 @@ public interface IEventStore
 }
 
 public record CommandHandlingResult(Command Command, IEnumerable<Event> Events);
+
+public static class EventStoreExtensions
+{
+    //TODO: Add error handling here
+    public static async Task AppendEventsAsync(this IEventStore store, IEnumerable<Event> events, ulong startingVersion, Command command)
+    {
+        ulong currentVersion = startingVersion;
+        foreach (var @event in @events)
+        {
+            var @eventToPublish = @event.ChangeVersion(currentVersion);
+            currentVersion = await store.AppendEventAsync(@eventToPublish);
+        }
+        var lastEvent = new CommandProcessedEvent(EventFactory.Shared.Create<CommandProcessedEvent>(command));
+        await store.AppendEventAsync(lastEvent.ChangeVersion(currentVersion));
+    }
+}
