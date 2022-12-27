1a8b586cd277d7f0a668179e75c34e9a96e1427c
korov <korova2321@gmail.com>
2022-12-27 03:04:21 +0200

    Add user search query

---
+++ src/back/Plants.Aggregates.Infrastructure/Search/UserSearchParamsProjector.cs
@@ -0,0 +1,80 @@
+﻿using Nest;
+using Plants.Aggregates.Search;
+using Plants.Aggregates.Users;
+
+namespace Plants.Aggregates.Infrastructure.Search;
+
+internal class UserSearchParamsProjector : ISearchParamsProjector<User, UserSearchParams>
+{
+    private readonly IIdentityProvider _identity;
+
+    public UserSearchParamsProjector(IIdentityProvider identity)
+    {
+        _identity = identity;
+    }
+
+    public SearchDescriptor<User> ProjectParams(UserSearchParams parameters, SearchDescriptor<User> desc) =>
+        desc.Query(
+            q => q.Bool(
+                b =>
+                {
+                    b.Must(
+                        u =>
+                    {
+                        if (String.IsNullOrEmpty(parameters.Name) is false)
+                        {
+                            AddNameFilter(parameters, u);
+                        }
+                        else
+                        {
+                            u.MatchAll();
+                        }
+                        return u;
+                    },
+                    u =>
+                    {
+                        if (String.IsNullOrEmpty(parameters.Phone) is false)
+                        {
+                            u.Fuzzy(f => f.Field(_ => _.PhoneNumber).Value(parameters.Phone));
+                        }
+                        else
+                        {
+                            u.MatchAll();
+                        }
+                        return u;
+                    },
+                    u =>
+                    {
+                        if (parameters.Roles is not null && parameters.Roles.Length > 0)
+                        {
+                            var roleNames = GetVisibleRoles(parameters.Roles).ToList();
+                            u.Terms(_ => _.Field(_ => _.Roles).Terms(roleNames));
+                        }
+                        else
+                        {
+                            u.MatchAll();
+                        }
+                        return u;
+                    });
+                    return b;
+                }
+                ));
+
+    private static void AddNameFilter(UserSearchParams parameters, QueryContainerDescriptor<User> query)
+    {
+        query.Bool(
+            b => b.Should(
+                u => u.Fuzzy(f => f.Field(_ => _.FirstName).Value(parameters.Name)),
+                u => u.Fuzzy(f => f.Field(_ => _.LastName).Value(parameters.Name))
+                )
+            );
+    }
+
+    private UserRole[] GetVisibleRoles(UserRole[] requestedRoles)
+    {
+        var roles = _identity.Identity!.Roles;
+        var lackingRoles = Enum.GetValues<UserRole>().Except(roles);
+        var result = requestedRoles.Except(lackingRoles).ToArray();
+        return result;
+    }
+}
