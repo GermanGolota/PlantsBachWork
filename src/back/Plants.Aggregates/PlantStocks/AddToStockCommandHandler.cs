8f827d7b6b040deec909e91aed27b49458d9489c
korov <korova2321@gmail.com>
2022-12-30 22:49:12 +0200

    Update plant stock to actually store images

---
+++ src/back/Plants.Aggregates/PlantStocks/AddToStockCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Plants.Aggregates.Services;
+
+namespace Plants.Aggregates.PlantStocks;
+
+internal class AddToStockCommandHandler : ICommandHandler<AddToStockCommand>
+{
+    private readonly IFileRepository _file;
+    private readonly IRepository<PlantStock> _repo;
+    private PlantStock _stock = null;
+
+    private const string _plantImageDirectory = "PlantImages";
+
+    public AddToStockCommandHandler(IFileRepository file, IRepository<PlantStock> repo)
+    {
+        _file = file;
+        _repo = repo;
+    }
+
+    public async Task<IEnumerable<Event>> HandleAsync(AddToStockCommand command)
+    {
+        _stock ??= await _repo.GetByIdAsync(command.Metadata.Aggregate.Id);
+        var files = await Task.WhenAll(command.Pictures.Select(picture => _file.SaveAsync(new(GetNewFileLocation(), picture))));
+        var urls = files.Select(_file.GetUrl).ToArray();
+        return new[]
+        {
+            new StockAddedEvent(EventFactory.Shared.Create<StockAddedEvent>(command), command.Plant, urls)
+        };
+    }
+
+    public async Task<CommandForbidden?> ShouldForbidAsync(AddToStockCommand command, IUserIdentity userIdentity)
+    {
+        _stock ??= await _repo.GetByIdAsync(command.Metadata.Aggregate.Id);
+        return userIdentity.HasRole(Producer).And(_stock.RequireNew);
+    }
+
+    private FileLocation GetNewFileLocation() =>
+        new FileLocation(Path.Combine(_plantImageDirectory, _stock.Id.ToString()), Guid.NewGuid().ToString(), "jpeg");
+}
---
+++ src/back/Plants.Shared/Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Plants.Shared.Extensions;
+
+public static class StreamExtensions
+{
+    public static async Task<byte[]> ReadAllBytesAsync(this Func<Stream> streamFunc)
+    {
+        using(var stream = streamFunc())
+        {
+            return await stream.ReadAllBytesAsync();
+        }
+    }
+
+    public static async Task<byte[]> ReadAllBytesAsync(this Stream stream)
+    {
+        var streamLength = Convert.ToInt32(stream.Length);
+        byte[] data = new byte[streamLength + 1];
+
+        //convert to to a byte array
+        await stream.ReadAsync(data, 0, streamLength);
+
+        return data;
+    }
+}
