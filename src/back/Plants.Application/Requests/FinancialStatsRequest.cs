f8c74d130d5c754fd8e54062db5f53de5d385f4a
korov <korova2321@gmail.com>
2023-01-02 03:30:29 +0200

    Introduce stats query

--- src/back/Plants.Application/Requests/FinancialStatsRequest.cs
+++ src/back/Plants.Application/Requests/FinancialStatsRequest.cs
@@ -8,7 +8,7 @@ public record FinancialStatsResult(IEnumerable<GroupFinancialStats> Groups);
 public class GroupFinancialStats
 {
     public decimal Income { get; set; }
-    public int GroupId { get; set; }
+    public long GroupId { get; set; }
     public string GroupName { get; set; }
     public long SoldCount { get; set; }
     public double PercentSold { get; set; }
--- src/back/Plants.Presentation/Controllers/StatsController.cs
+++ src/back/Plants.Presentation/Controllers/StatsController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Plants.Aggregates.PlantInfos;
 using Plants.Aggregates.PlantStats;
 using Plants.Application.Requests;
 
@@ -43,22 +44,42 @@ public class StatsController : ControllerBase
 public class StatsControllerV2 : ControllerBase
 {
     private readonly IProjectionQueryService<PlantStat> _statQuery;
+    private readonly IProjectionQueryService<PlantInfo> _infoQuery;
 
-    public StatsControllerV2(IProjectionQueryService<PlantStat> statQuery)
+    public StatsControllerV2(IProjectionQueryService<PlantStat> statQuery, IProjectionQueryService<PlantInfo> infoQuery)
     {
         _statQuery = statQuery;
+        _infoQuery = infoQuery;
     }
 
     [HttpGet("financial")]
-    public async Task<ActionResult<FinancialStatsResult>> Financial([FromQuery] DateTime from, [FromQuery] DateTime to, CancellationToken token)
+    public async Task<ActionResult<FinancialStatsResult>> Financial([FromQuery] DateTime? from, [FromQuery] DateTime? to, CancellationToken token)
     {
-
-        throw new NotImplementedException();
+        var stats = await _statQuery.FindAllAsync(_ => true);
+        var groups = (await _infoQuery.GetByIdAsync(PlantInfo.InfoId)).GroupNames.ToInverse();
+        List<GroupFinancialStats> results = new();
+        foreach (var stat in stats)
+        {
+            results.Add(new()
+            {
+                GroupId = groups[stat.GroupName],
+                GroupName = stat.GroupName,
+                Income = stat.IncomeUpdated.Where(_ => IsInRange(_.Time, from, to)).Sum(_ => _.Value),
+                SoldCount = stat.SoldCountUpdated.Where(_ => IsInRange(_, from, to)).Count(),
+                PercentSold = stat.SoldCountUpdated.Where(_ => IsInRange(_, from, to)).Count() / stat.PostedCountUpdated.Where(_ => _ > from && _ < to).Count()
+            });
+        }
+        return new FinancialStatsResult(results);
     }
+
+    private bool IsInRange(DateTime time, DateTime? from, DateTime? to) =>
+        (from is null || time > from) && (to is null || time < to);
 
     [HttpGet("total")]
     public async Task<ActionResult<TotalStatsResult>> Total(CancellationToken token)
     {
-        throw new NotImplementedException();
+        var stats = await _statQuery.FindAllAsync(_ => true);
+        var groups = (await _infoQuery.GetByIdAsync(PlantInfo.InfoId)).GroupNames.ToInverse();
+        return new TotalStatsResult(stats.Select(stat => new GroupTotalStats(groups[stat.GroupName], stat.GroupName, stat.Income, stat.InstructionsCount, stat.PlantsCount)));
     }
 }
