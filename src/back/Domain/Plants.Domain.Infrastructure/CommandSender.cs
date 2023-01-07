68b63c5f8b87e144aa381f2a4ba1a43a72c4960a
korov <korova2321@gmail.com>
2023-01-07 19:38:00 +0200

    Add success log

--- src/back/Domain/Plants.Domain.Infrastructure/CommandSender.cs
+++ src/back/Domain/Plants.Domain.Infrastructure/CommandSender.cs
@@ -92,6 +92,7 @@ internal class CommandSender : ICommandSender
             {
                 events = await ExecuteHandlersAsync(command, checkResults, token);
                 await _eventStore.AppendEventsAsync(events, commandVersion, command, token);
+                _logger.LogInformation("Successfully processes command '{commandId}' for '{aggregate}'", command.Metadata.Id, command.Metadata.Aggregate);
                 result = new CommandAcceptedResult();
             }
             catch (Exception e)
