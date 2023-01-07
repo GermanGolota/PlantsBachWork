3fa360f358801be59c2307a0ced543ed20b68313
korov <korova2321@gmail.com>
2023-01-07 18:30:15 +0200

    Configure serilog

--- src/back/Plants.Presentation/Program.cs
+++ src/back/Plants.Presentation/Program.cs
@@ -1,13 +1,20 @@
-using Plants.Aggregates.Infrastructure.Abstractions;
 using Plants.Presentation;
+using Serilog;
 
 var host = Host.CreateDefaultBuilder(args)
         .ConfigureWebHostDefaults(webBuilder =>
         {
             webBuilder.UseStartup<Startup>();
         })
+        .UseSerilog()
         .Build();
 
+var config = host.Services.GetRequiredService<IConfiguration>();
+Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+//Serilog.Debugging.SelfLog.Enable(Console.WriteLine);
+
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-var context = host.Services.GetRequiredService<IHostingContext>();
 host.Run();
