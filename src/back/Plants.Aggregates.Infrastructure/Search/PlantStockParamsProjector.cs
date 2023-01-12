a80795ce026a33a595bc6ffeebd20467b8304e42
korov <korova2321@gmail.com>
2023-01-13 00:33:16 +0200

    Fix plant stock query

--- src/back/Plants.Aggregates.Infrastructure/Search/PlantStockParamsProjector.cs
+++ src/back/Plants.Aggregates.Infrastructure/Search/PlantStockParamsProjector.cs
@@ -13,11 +13,11 @@ internal class PlantStockParamsProjector : ISearchParamsProjector<PlantStock, Pl
         _identityProvider = identityProvider;
     }
 
-    public SearchDescriptor<PlantStock> ProjectParams(PlantStockParams parameters, SearchDescriptor<PlantStock> desc)
-    {
-        var username = _identityProvider.Identity!.UserName;
-        return desc.Query(q => parameters.IsMine 
-            ? q.Term(t => t.Field(f => f.Caretaker.Login).Value(username))
-            : q.MatchAll());
-    }
-} 
+    public SearchDescriptor<PlantStock> ProjectParams(PlantStockParams parameters, SearchDescriptor<PlantStock> desc) =>
+        desc.Query(q =>
+        q.Bool(b => b.Must(
+                _ => parameters.IsMine
+                ? _.Term(t => t.Field(f => f.Caretaker.Login).Value(_identityProvider.Identity!.UserName))
+                : _.MatchAll(),
+                _ => _.Term(f => f.Field(_ => _.BeenPosted).Value(false)))));
+}
