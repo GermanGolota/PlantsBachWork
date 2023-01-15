d4ea70d24eb9c9128f89a2db12dcbf5860c7fd95
korov <korova2321@gmail.com>
2023-01-15 14:34:25 +0200

    Introduce timeout exception

--- src/back/Plants.Initializer/HealthCheck/HealthChecker.cs
+++ src/back/Plants.Initializer/HealthCheck/HealthChecker.cs
@@ -23,37 +23,37 @@ internal class HealthChecker
         var timer = Stopwatch.StartNew();
         _logger.LogInformation("Waiting for services to startup for up to '{sec}' seconds", _options.TimeoutInSeconds);
 
-        var timeoutTask = Task.Delay(TimeSpan.FromSeconds(_options.TimeoutInSeconds), token);
-        var healthCheckTask = Task.Run(async () =>
+        var executed = await WaitForHealthCheckAsync(token).ExecuteWithTimeoutAsync(TimeSpan.FromSeconds(_options.TimeoutInSeconds));
+        if (executed)
         {
-            while (true)
-            {
-                var report = await _health.CheckHealthAsync(token);
-                if (report.Status == HealthStatus.Healthy || report.Status == HealthStatus.Degraded && _options.AcceptDegraded)
-                {
-                    break;
-                }
-                else
-                {
-                    _logger.LogInformation("Health check returned status '{status}'", report.Status);
-                    foreach (var (source, entry) in report.Entries)
-                    {
-                        _logger.LogInformation("'{source}': '{status}' stating '{desc}'", source, entry.Status, entry.Description);
-                    }
-                    await Task.Delay(TimeSpan.FromSeconds(_options.PollIntervalInSeconds));
-                }
-            }
-        }, token);
-        var resultingTask = await Task.WhenAny(timeoutTask, healthCheckTask);
-        if (resultingTask == timeoutTask)
+            timer.Stop();
+            _logger.LogInformation("Services sucessfully started in '{time}'!", timer.Elapsed);
+        }
+        else
         {
             _logger.LogError("Timeout out after '{sec}' seconds while waiting for services to become available", _options.TimeoutInSeconds);
             throw new Exception("Some services failed to come up on time");
         }
-        else
+    }
+
+    private async Task WaitForHealthCheckAsync(CancellationToken token)
+    {
+        while (true)
         {
-            timer.Stop();
-            _logger.LogInformation("Services sucessfully started in '{time}'!", timer.Elapsed);
+            var report = await _health.CheckHealthAsync(token);
+            if (report.Status == HealthStatus.Healthy || report.Status == HealthStatus.Degraded && _options.AcceptDegraded)
+            {
+                break;
+            }
+            else
+            {
+                _logger.LogInformation("Health check returned status '{status}'", report.Status);
+                foreach (var (source, entry) in report.Entries)
+                {
+                    _logger.LogInformation("'{source}': '{status}' stating '{desc}'", source, entry.Status, entry.Description);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(_options.PollIntervalInSeconds));
+            }
         }
     }
 }
---
+++ src/back/Plants.Shared/Extensions/TaskExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plants.Shared.Extensions;
+
+public static class TaskExtensions
+{
+    /// <returns>
+    /// Value indicating success of execution:
+    /// true - task executed
+    /// false - timed out
+    /// </returns>
+    public static async Task<bool> ExecuteWithTimeoutAsync(this Task task, TimeSpan timeout)
+    {
+        var timeoutTask = Task.Delay(timeout);
+        var resultingTask = await Task.WhenAny(timeoutTask, task);
+        bool success;
+        if (resultingTask == timeoutTask)
+        {
+            success = false;
+        }
+        else
+        {
+            success = true;
+        }
+        return success;
+    }
+}
