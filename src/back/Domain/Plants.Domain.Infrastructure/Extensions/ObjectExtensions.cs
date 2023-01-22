9807361ea3c2a870b5c40189ad7f32920596ea55
korov <korova2321@gmail.com>
2023-01-22 04:42:28 +0200

    Add remove property method

---
+++ src/back/Domain/Plants.Domain.Infrastructure/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Dynamic;
+
+namespace Plants.Domain.Infrastructure.Extensions;
+
+internal static class ObjectExtensions
+{
+    internal static object RemoveProperty(this object obj, string name)
+    {
+        var jobj = JObject.FromObject(obj);
+        jobj.Property(name)!.Remove();
+        var expanded = jobj.ToObject<ExpandoObject>()!;
+        dynamic dynamicData = expanded;
+        return dynamicData;
+    }
+}
