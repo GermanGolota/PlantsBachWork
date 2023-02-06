4f2752232a394b1d3b773ae3727ffb82888b1727
korov <korova2321@gmail.com>
2023-02-06 17:27:51 +0200

    Refactor extension

--- src/back/Plants.Presentation/Dtos/CommandViewResult.cs
+++ src/back/Plants.Presentation/Dtos/CommandViewResult.cs
@@ -4,11 +4,9 @@ public record CommandViewResult(bool Success, string Message);
 
 public static class CommandViewResultExtensions
 {
-    public static CommandViewResult ToCommandResult(this OneOf<CommandAcceptedResult, CommandForbidden> cmdResult, string successMessage = "Successfull")
-    {
-        return cmdResult.Match(
-            _ => new CommandViewResult(true, successMessage), 
+    public static CommandViewResult ToCommandResult(this OneOf<CommandAcceptedResult, CommandForbidden> cmdResult, string successMessage = "Successfull") =>
+        cmdResult.Match(
+            _ => new CommandViewResult(true, successMessage),
             fail => new CommandViewResult(false, string.Join('\n', fail.Reasons))
             );
-    }
 }
