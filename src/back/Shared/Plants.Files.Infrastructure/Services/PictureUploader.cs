1b66c01d5e927b925af12c966b4034366d076fd6
korov <korova2321@gmail.com>
2023-04-11 01:31:10 +0300

    Compress all of the images

--- src/back/Shared/Plants.Files.Infrastructure/Services/PictureUploader.cs
+++ src/back/Shared/Plants.Files.Infrastructure/Services/PictureUploader.cs
@@ -1,4 +1,6 @@
 ﻿using Plants.Aggregates;
+using Plants.Shared;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace Plants.Files.Infrastructure;
 
@@ -19,9 +21,26 @@ internal sealed class PictureUploader : IPictureUploader
 
     public async Task<Picture> UploadAsync(FileView file, CancellationToken token)
     {
+        var finalContent = await GetFinalContent(file.Content);
         var fileId = file.Id;
         var location = new FileLocation("Images", fileId.ToString(), "jpeg");
-        await _file.SaveAsync(new(location, file.Content), token);
+        await _file.SaveAsync(new(location, finalContent), token);
         return new Picture(fileId, _file.GetUrl(location));
+    }
+
+    private static async Task<byte[]> GetFinalContent(byte[] bytes)
+    {
+        using (var image = Image.Load(bytes))
+        {
+            const int width = 512;
+            const int height = 512;
+            image.Mutate(x => x.Resize(width, height));
+
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, new JpegEncoder());
+                return await ms.ReadAllBytesAsync();
+            }
+        }
     }
 }
